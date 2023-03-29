1109b7f0a912586a480276a701a244d3d48cf2f3
Daemon <clement.lecoeur34@gmail.com>
2023-03-29 14:44:54 +0200

    Moving const, include and defines to new file defines.hpp

--- Sources/VulkanRenderer/main.cpp
+++ Sources/VulkanRenderer/main.cpp
@@ -1,30 +1,4 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
-#include <chrono>
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <cstdlib>
-#include <cstdint>
-#include <limits>
-#include <array>
-#include <optional>
-#include <set>
-
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
-const int MAX_FRAMES_IN_FLIGHT = 2;
+#include "defines.hpp"
 
 struct Vertex {
     glm::vec2 pos;
