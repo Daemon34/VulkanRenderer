a6e2e471d7b21035eb9714c06b8277eaeda41d7c
Daemon <clement.lecoeur34@gmail.com>
2023-04-11 17:51:29 +0200

    Adding shiba model

--- Sources/VulkanRenderer/defines.hpp
+++ Sources/VulkanRenderer/defines.hpp
@@ -5,7 +5,7 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
+const std::string MODEL_PATH = "models/shiba.obj";
+const std::string TEXTURE_PATH = "textures/shiba_Base_Color.png";
 
 const int MAX_FRAMES_IN_FLIGHT = 2;
